ec576c28c5380c63454ad3c472d02c0ca412b1fc
Avishkar Bahirwar <aavishkar.tl@gmail.com>
2025-04-11 14:28:12 +0530

    WSL changes

--- ftg_code/ftg.py
+++ ftg_code/ftg.py
@@ -8,12 +8,12 @@ from sensor_msgs.msg import LaserScan
 from ackermann_msgs.msg import AckermannDriveStamped, AckermannDrive
 
 #global a
-fov_l=20
-fov_r=160
+fov_l=0
+fov_r=270
 rb=0.3
 vel_max=2
 alpha=0.1
-k=0.1
+k=0.0001
 
 class LidarProcessingNode(Node):
     def __init__(self):
@@ -25,8 +25,8 @@ class LidarProcessingNode(Node):
         self.ackermann_publisher = self.create_publisher(AckermannDriveStamped, 'drive', 10)
 
     def lidar_callback(self,msg):
-        angles,length=self.angle_assignment(msg)
-        smoothened_array=self.preprocess(msg,angles)
+        angles=self.angle_assignment(msg)
+        smoothened_array,length=self.preprocess(msg,angles)
         new_array = self.chopped_array(smoothened_array,length)
         index ,sign= self.parity(new_array)
         corrected_angle = self.correction(new_array, index,sign)
@@ -36,25 +36,27 @@ class LidarProcessingNode(Node):
         vel = self.throttle(phi_gap)
 
         ackermann_msg = AckermannDriveStamped()
-        ackermann_msg.drive.steering_angle = phi_gap*(np.pi/180)  
+        ackermann_msg.drive.steering_angle = -1*(135-phi_gap)*(np.pi/180)  
         ackermann_msg.drive.steering_angle_velocity = turn_vel  
         ackermann_msg.drive.speed = vel  
         self.ackermann_publisher.publish(ackermann_msg)
 
-        self.get_logger().info(f"Phi Gap: {phi_gap}, Turn Vel: {turn_vel}, Velocity: {vel}")
+        self.get_logger().info(f"Phi Gap: {135-phi_gap}, Turn Vel: {turn_vel}, Velocity: {vel}")
             
         
     def angle_assignment(self,msg,angle_min=0,angle_max=4.71239):
-        length = len(np.array(msg.ranges))
+        #length = len(np.array(msg.ranges))
         angles = np.linspace(angle_min,angle_max,len(np.array(msg.ranges))) * 180 / np.pi
-        return angles,length
+        return angles
 
     def preprocess(self, msg,angles):
         ranges=np.array(msg.ranges)
         smoothened_array=[]
         for i in range(0, len(ranges)):
             smoothened_array.append((ranges[i],angles[i]))
-        return smoothened_array
+        smoothened_array=smoothened_array[::20]
+        length=len(smoothened_array)
+        return smoothened_array,length
 
     def chopped_array(self,l,length):
         new_list = []
@@ -70,15 +72,15 @@ class LidarProcessingNode(Node):
         # l=lidar_data()
         # Used to extract the distance from the tuple and calculate the difference
         diff = np.diff([t[0] for t in new_array])
-        threshold = 0.02 # Used to set a particular threshold for discontinuity identification
+        threshold = 0.5 # Used to set a particular threshold for discontinuity identification
         index = np.where(np.abs(diff) > threshold)[0]
         if diff[index[0]]<0:
             sign=-1
         else:
             sign=1
         # index1=np.where(-diff>threshold)[0]
-        print(diff)
-        print(index)
+        #print(diff)
+        #print(index)
         # print (index1)
         #print(new_array[index[1]][1])
         # print(index1+1)
@@ -88,17 +90,18 @@ class LidarProcessingNode(Node):
         corrected_angle = []
         for i in index:
             corr = (rb/new_array[i][0])*180/3.14
+            corr=0
             corrected_angle.append(new_array[i][1]+(sign*corr))
             sign=sign*-1
 
 
-        print(corrected_angle)
+        #print(corrected_angle)
         return corrected_angle
     
     def gap_array(self, corrected_angle):
         #ga = np.concatenate(([fov_l], corrected_angle, [fov_r]))
         ga=corrected_angle
-        print(ga)
+        #print(ga)
         final_ga = np.diff(ga)
         phi1_index = np.argmax(final_ga)
         phi2_index = phi1_index+1
@@ -106,25 +109,28 @@ class LidarProcessingNode(Node):
         phi2 = ga[phi2_index]
         print(phi1)
         print(phi2)
-        print(final_ga)
+        #print(final_ga)
         return phi1, phi2, phi1_index
     
     def heading_angle(self, phi1, phi2, new_array, phi_index):
+        
         d1 = new_array[phi_index][0]
         d2 = new_array[phi_index+1][0]
         print(d1, d2)
+        """
         denominator = np.sqrt(d1*d1+d2*d2+2*d1*d2*np.cos(np.radians(phi2-phi1)))
         phi_gap = np.degrees(
-            np.arccos((d1+(d2*(np.cos(np.radians(phi2-phi1)))))/denominator))+phi1
-
+            np.arccos((d1+(d2*(np.cos(np.radians(phi2-phi1)))))/denominator))+phi1-45
+            """
+        phi_gap=(phi1+phi2)/2
         return phi_gap
     
     def throttle(self, phi_gap):
-        vel = vel_max*(1-math.exp(-1*alpha*phi_gap))
+        vel = 0.1+vel_max*(1-math.exp(-1*alpha*(phi_gap*np.pi/180)))
         return vel
     
     def turn(self, phi_gap):
-        turn_vel = k*phi_gap
+        turn_vel =1+ k*(phi_gap*np.pi/180)
         return turn_vel
         
 
